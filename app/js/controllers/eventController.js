a5b6316ebc67ff659d32bf629e8ea47a410aa4e6
Nick Rodriguez <Nick.Rodriguez@energytrust.org>
2016-01-29 16:29:25 -0800

    added controller

--- app/js/controllers/eventController.js
+++ app/js/controllers/eventController.js
@@ -14,13 +14,19 @@
          var vm = this;
 
          vm.event = {
-            name: 'Derp'
-
-        };
-
+            name: 'Time To Derp',
+            date: '1/1/2018',
+            time:'4:20 PM',
+            location:
+            {
+                address: 'Google Headquarters',
+                city: 'Mountain View',
+                state: 'CA'
+            },
+            imageUrl: '/img/hipster-kitty.png'
 
      }
-
+}
 
 
 
